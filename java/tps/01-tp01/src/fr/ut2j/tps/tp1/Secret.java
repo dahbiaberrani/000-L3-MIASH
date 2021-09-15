cc40a6f74d4084ba95fff59fc08f1afa84cacce0
Dahbia BERRANI-EPS-HADDAD <Dahbia.BERRANI-EPS-HADDAD@crie.i-univ-tlse2.fr>
2021-09-15 16:09:39 +0200

    commence exercice 4

---
+++ java/tps/01-tp01/src/fr/ut2j/tps/tp1/Secret.java
@@ -0,0 +1,13 @@
+package fr.ut2j.tps.tp1;
+/*
+ * Université Toulouse 2 Jean Jaures
+ * L3 MIASHS 2021-2022
+ * BERRANI Dahbia
+ * berrani.dehbia1993@gmail.com
+ */
+
+public class Secret {
+    public static boolean lecteur(int nombre){
+
+    }
+}
