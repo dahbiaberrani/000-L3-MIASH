05ef1d1bc87aa1b29e5a595291fc2fdead3392dd
Dahbia BERRANI <dehbia.berrani1993@gmail.com>
2022-02-05 17:38:48 +0100

    c++ exercice1 fini
    Signed-off-by: Dahbia BERRANI <dehbia.berrani1993@gmail.com>

--- c++/tps/tp2/exercice1/main.cpp
+++ c++/tps/tp2/exercice1/main.cpp
@@ -1,14 +1,20 @@
-#include "Cpersonne.hpp"
+#include "CPersonne.hpp"
 #include <iostream>
 
 using namespace std;
 
 int main() {
-   
-    // Cpersonne personne1 = Cpersonne("85485", "toto","titi", "email@domaine.com");
-    Cpersonne personne1 =  Cpersonne();
-    personne1.set_id("4585");
-    cout << personne1.get_id() << endl;
+ 
+    CPersonne p1 { " 1 " ," Trojahn " ," Cassia " ," cassia . trojahn@irit . fr " };
+    cout << "affichage de p1 " << p1;
+    CPersonne p2 = p1 ;
+    cout << "p2 = p1 donc p2 :" << endl << p2;
+    CPersonne p3 ;
+    p3 = p1 ;
+    cout << "p3 = p1 donc p3 :" << endl << p3;
 
+    CPersonne p4 ;
+    p4 = CPersonne ( " 2 " ," Clemente " ," Gilles " ," gilles@cc . fr " ) ;
+    cout << "affichage de p4 " << endl << p4;
     return 0;
 }
---
+++ c++/tps/tp2/exercice2/CNoeud.hpp
@@ -0,0 +1,33 @@
+
+// #include "../tp1/exercice2";
+
+
+
+// class CNoeud {
+//     private :
+//     CPersonne * pers ;
+//     CNoeud * pred ;
+//     CNoeud * suiv ;
+//     public :CNoeud () ;
+//     CNoeud ( CPersonne * , CNoeud * , CNoeud *) ;
+//     CPersonne * get_pers () const ;
+//     CNoeud * get_pred () const ;
+//     CNoeud * get_suiv () const ;
+//     ~ CNoeud () ;
+// };
+
+// class CListe {
+//     CNoeud * tete ;
+//     CNoeud * queue ;
+//     public :
+//     CListe () ;
+//     CListe ( CNoeud * , CNoeud *) ;
+//     bool ajoute ( CPersonne *) ;
+//     CPersonne * recherche ( string ) const ;
+//     CPersonne * supprime ( string ) ;
+//     bool vide () const ;
+//     void affiche () const ;
+//     CNoeud * get_tete () const ;
+//     CNoeud * get_queue () const ;
+//     ~ CListe () ;
+// };
