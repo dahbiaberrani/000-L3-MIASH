91344de87bdda918d680ee7c4b47059c59375dd9
Dahbia BERRANI <dehbia.berrani1993@gmail.com>
2022-02-05 17:41:33 +0100

    c++ exercice1 fini
    Signed-off-by: Dahbia BERRANI <dehbia.berrani1993@gmail.com>

---
+++ c++/tps/tp2/exercice1/CPersonne.cpp
@@ -0,0 +1,140 @@
+#include "CPersonne.hpp"
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+CPersonne::CPersonne() {
+    id = nullptr;
+    nom = nullptr;
+    prenom = nullptr;
+    mail = nullptr;
+}
+
+CPersonne::CPersonne(string id, string nom, string prenom, string mail) {
+    CPersonne::id = new string(id);
+    CPersonne::nom = new string(nom);
+    CPersonne::prenom = new string(prenom);
+    CPersonne::mail = new string(mail);
+}
+
+CPersonne::~CPersonne() {
+    if (this->id != nullptr) {
+        delete id;
+        id = nullptr;
+    } 
+
+    if (this->nom!= nullptr) {
+        delete nom;
+        nom = nullptr;
+    }  
+
+    if (this->prenom != nullptr) {
+        delete CPersonne::prenom;
+        prenom = nullptr;
+    }   
+
+    if (this->mail != nullptr) {
+        delete CPersonne::mail;
+        mail = nullptr;
+    }   
+}
+
+void CPersonne::set_id(string _id) {
+    if (this->id != nullptr) {
+        delete CPersonne::id;
+    }
+    this->id = new string(_id);
+}
+
+void CPersonne::set_nom(string _nom) {
+    if (this->nom!= nullptr) {
+        delete CPersonne::nom;
+    }
+    this->nom = new string(_nom);
+}
+
+void CPersonne::set_prenom(string _prenom) {
+    if (this->prenom != nullptr) {
+        delete CPersonne::prenom;
+    }
+    this->prenom = new string(_prenom);
+}
+
+void CPersonne::set_mail(string email) {
+    if (this->mail != nullptr) {
+        delete CPersonne::mail;
+    }
+    this->mail = new string(email);
+}
+
+
+string CPersonne::get_id() {
+    return *(this->id);
+}
+
+string CPersonne::get_nom() {
+    return *(this->nom);
+}
+
+string CPersonne::get_prenom() {
+    return *(this->prenom);
+}
+
+string CPersonne::get_mail() {
+    return *(this->mail);
+}
+
+bool CPersonne::operator==(const CPersonne &other_per) {
+    return id == other_per.id &&
+           nom == other_per.nom &&
+           prenom == other_per.prenom &&
+           mail == other_per.mail;
+}
+
+ostream& operator<<(ostream& os, const CPersonne& persone) {
+    os << *persone.id << endl << *persone.nom << endl << *persone.prenom << endl << *persone.mail << endl << "---------------------" << endl;
+    return os;
+}
+
+
+CPersonne::CPersonne(const CPersonne& other_personne) {
+    id = new string(*other_personne.id);
+    nom = new string(*other_personne.nom);
+    prenom = new string(*other_personne.prenom);
+    mail = new string(*other_personne.mail);
+}
+
+CPersonne& CPersonne::operator=(const CPersonne& other_personne){
+    if (this != &other_personne) {
+        // suppresion des anciens elements
+        if (this->id != nullptr) {
+            delete id;
+            id = nullptr;
+        } 
+
+        if (this->nom!= nullptr) {
+            delete nom;
+            nom = nullptr;
+        }  
+
+        if (this->prenom != nullptr) {
+            delete CPersonne::prenom;
+            prenom = nullptr;
+        }   
+
+        if (this->mail != nullptr) {
+            delete CPersonne::mail;
+            mail = nullptr;
+        }   
+
+        // initialisation et copie
+
+        id = new string(*other_personne.id);
+        nom = new string(*other_personne.nom);
+        prenom = new string(*other_personne.prenom);
+        mail = new string(*other_personne.mail);             
+    }
+    return *this;
+
+}
---
+++ c++/tps/tp2/exercice1/CPersonne.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+using namespace std;
+
+
+class CPersonne {
+    private:
+        string *id, *nom, *prenom, *mail;
+
+    public:
+        CPersonne();
+        CPersonne(string id,string nom,string prenom,string mail);
+        void set_id(string id);
+        void set_nom(string nom);
+        void set_prenom(string prenom);
+        void set_mail(string mail);
+        string get_id();
+        string get_nom();
+        string get_prenom();
+        string get_mail();
+        ~CPersonne();
+        friend ostream& operator<<(ostream& os, const CPersonne& persone);
+        bool operator==(const CPersonne &other_per);
+        CPersonne(const CPersonne& other_personne);
+        CPersonne& operator=(const CPersonne& other_personne);
+        
+};
