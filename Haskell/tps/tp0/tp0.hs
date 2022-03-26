b21d452221376d0dec569f6d1431ac3786926d00
Dahbia BERRANI <dehbia.berrani1993@gmail.com>
2022-03-26 20:35:04 +0100

    soustraction fonctionne

--- Haskell/tps/tp0/tp0.hs
+++ Haskell/tps/tp0/tp0.hs
@@ -1,5 +1,15 @@
 plus :: Int -> Int -> Int
 plus x 0 = x
 plus x y
-    | y > 0 = plus (succ x) (pred y) -- Incr´emente x y fois
-    | otherwise = plus (pred x) (succ y) -- D´ecr´emente x y fois
+    | y > 0 = plus (succ x) (pred y) 
+    | otherwise = plus (pred x) (succ y) 
+
+
+
+soustraction :: Int -> Int -> Int
+soustraction  x 0 = x
+soustraction x y 
+    | y >0 = soustraction (pred x) (pred y)
+    | otherwise = soustraction (succ x) (succ y)
+
+
