854732892d80410f204e72f5826db2cf16a362b7
Dahbia BERRANI <dehbia.berrani1993@gmail.com>
2022-03-26 22:32:26 +0100

    mult fonctionne

--- Haskell/tps/tp0/tp0.hs
+++ Haskell/tps/tp0/tp0.hs
@@ -9,7 +9,17 @@ plus x y
 soustraction :: Int -> Int -> Int
 soustraction  x 0 = x
 soustraction x y 
-    | y >0 = soustraction (pred x) (pred y)
+    | y > 0 = soustraction (pred x) (pred y)
     | otherwise = soustraction (succ x) (succ y)
 
 
+soustraction1 :: Int -> Int -> Int
+soustraction1  x y = plus x (-y)
+
+
+mult :: Int -> Int -> Int
+mult x 0 = 0
+mult x y 
+    | y > 0 = plus x (mult x (pred y))
+    -- | y < 0 = plus (-x) (mult x (succ y))
+    | otherwise  = mult (-x) (-y)
