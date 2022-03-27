eadca593b1d4dfba78b0dc41c5bb69e9f77afbb3
Dahbia BERRANI <dehbia.berrani1993@gmail.com>
2022-03-27 12:19:25 +0200

    pair et impair marche
    Signed-off-by: Dahbia BERRANI <dehbia.berrani1993@gmail.com>

--- Haskell/tps/tp0/tp0.hs
+++ Haskell/tps/tp0/tp0.hs
@@ -31,3 +31,24 @@ quotient x y
 
 reste :: Int -> Int -> Int 
 reste x y = soustraction x (mult(quotient x y) y)
+
+estPair :: Int -> Bool
+estPair 0 = True
+estPair 1 = False 
+estPair x = estPair (soustraction x 2)
+
+estImpair :: Int -> Bool 
+estImpair 0 = False 
+estImpair 1 = True 
+estImpair x = estImpair (soustraction x 2)
+
+estImpair2 :: Int  -> Bool 
+estImpair2 x = not (estPair x)
+
+
+pow :: Int -> Int -> Int 
+pow x 0 = 1
+pow x 1 = x
+pow x n 
+    | estPair n = pow (mult x x) (quotient n 2)
+    | estImpair2 n = mult x (pow (mult x x) (quotient (soustraction n 1) 2))
